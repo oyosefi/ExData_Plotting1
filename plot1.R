e76135481dd7d55d7efd052a1758b4018c95fecf
Ori Yosefi <oriy@microsoft.com>
2015-01-10 22:13:27 +0200

    first plot

---
+++ plot1.R
@@ -0,0 +1,5 @@
+fullData <- read.csv("./household_power_consumption.txt", sep=";", na.strings="?")
+theData <- subset(fullData, Date == "1/2/2007"| Date=="2/2/2007" ,select = c(Date, Global_active_power))
+png(filename="plot1.png", width=480, height=480)
+hist(theData$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red", ylim=c(0,1200))
+dev.off()
